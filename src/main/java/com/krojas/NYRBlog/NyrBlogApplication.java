83841eb08c34e743d76f8dfe88f58442f013caa1
Karel Rojas Requena <karelrojas64@gmail.com>
2024-05-14 09:56:15 -0400

    created inital spring boot files

---
+++ src/main/java/com/krojas/NYRBlog/NyrBlogApplication.java
@@ -0,0 +1,13 @@
+package com.krojas.NYRBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyrBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyrBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krojas/NYRBlog/NyrBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krojas.NYRBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyrBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
