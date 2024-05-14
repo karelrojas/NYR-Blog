876b0600ff049fb8421220ba2749701214536721
Karel Rojas Requena <karelrojas64@gmail.com>
2024-05-14 18:02:48 -0400

    created skeleton main page for blogsite

---
+++ blog-site/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header">
+        <div className="Title">
+          Placeholder Title
+        </div>
+        <div className="Page-links">
+          <div className="Link 1">Blog</div>
+          <div className="Link 2">Statistics</div>
+          <div className="Link 3">News</div>
+        </div>
+      </div>
+      <div className="Upcoming">
+        <div className="Current-record">
+
+        </div>
+        <div className="Upcoming-games">
+
+        </div>
+      </div>
+      <div className="Main-body">
+        <div className="Statistics">
+          <div className="Player-highlight">
+            Placeholder Player
+          </div>
+          <div className="Quick-stats">
+            Placeholder Stats
+          </div>
+        </div>
+        <div className="Blog-preview">
+          Placeholder Blog Section
+        </div>
+        <div className="External">
+          <div className="Articles">
+            Placeholder Articles
+          </div>
+          <div className="E-links">
+            Placeholder E-links
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
