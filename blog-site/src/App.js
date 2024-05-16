0fab5c425691adcca5e8f1439c0112a9c0a5a35c
Karel Rojas Requena <karelrojas64@gmail.com>
2024-05-16 14:32:41 -0400

    recent game info displays on main page via spring boot

--- blog-site/src/App.js
+++ blog-site/src/App.js
@@ -1,6 +1,17 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [gamesList, setGamesList] = useState([]);
+
+  async function getGames(){
+    const res = await axios.get('http://localhost:8082/games');
+    console.log(res.data.gamesByDate);
+    setGamesList(res.data.gamesByDate);
+  }
+
   return (
     <div className="App">
       <div className="Header">
@@ -18,7 +29,21 @@ function App() {
 
         </div>
         <div className="Upcoming-games">
-
+          <button onClick={getGames}>Get Games</button>
+           {gamesList.map((gameInfo, index) => (
+            <div key={index} className="gameInfo">
+            {gameInfo.games.map((singleGame, ind) => (
+              <div key={ind} className="singleGame">
+                <div className="game gameDate">{singleGame.gameDate}</div>
+                <div className="game awayTeam">{singleGame.awayTeam.abbrev}</div>
+                <div className="game score">{singleGame.awayTeam.score}</div>
+                <div className="game homeTeam">{singleGame.homeTeam.abbrev}</div>
+                <div className="game score">{singleGame.homeTeam.score}</div>
+              </div>
+            ))}
+            </div>
+          ))} 
+          <div className="example"></div>
         </div>
       </div>
       <div className="Main-body">
